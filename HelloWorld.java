8eb0876091be70033eee38f1a10625beb26b03e8
tslemke11 <tyleris@gmail.com>
2016-05-13 11:19:04 -0600

    First commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+void display() {
+  System.out.println("Hello World");
+}
+
+public static void main(String args[]) {
+
+  HelloWorld h = new HelloWorld();
+  h.display();
+
+}
+
+}
